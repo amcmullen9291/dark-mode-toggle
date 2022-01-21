60c07971100d714bf494d8c05430ef630853f140
amcmullen <amcmullen9291@gmail.com>
2022-01-20 20:21:27 -0500

    yarn dependencies

---
+++ src/Components/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <>
+        <div>
+            <div id="outerBox">
+                <div id="innerBox">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla urna porttitor rhoncus dolor purus non enim praesent elementum. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Dapibus ultrices in iaculis nunc sed. Lobortis elementum nibh tellus molestie nunc. Sed arcu non odio euismod lacinia at quis risus. Arcu ac tortor dignissim convallis aenean et tortor at risus. Ornare suspendisse sed nisi lacus sed. Sodales neque sodales ut etiam sit amet nisl purus in. Sed euismod nisi porta lorem. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Risus ultricies tristique nulla aliquet enim.
+
+    Turpis egestas sed tempus urna et. Suspendisse sed nisi lacus sed viverra tellus. Congue nisi vitae suscipit tellus mauris a diam. Enim nec dui nunc mattis enim ut. Amet purus gravida quis blandit turpis. Id aliquet risus feugiat in ante metus. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. In fermentum posuere urna nec. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Tristique senectus et netus et. Faucibus turpis in eu mi bibendum. Lectus arcu bibendum at varius.
+                </div>
+            </div>
+        </div>
+        </>  
+    );
+};
+
+export default HomePage;
